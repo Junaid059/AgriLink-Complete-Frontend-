b810a11abd254b60c1e0daf7b7113d3a60216611
junaid khalid <junaidkhalid2001@gmail.com>
2024-11-30 15:15:00 +0500

    updated

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -39,7 +39,6 @@ function ProtectedRoute({ children }) {
 function AppLayout() {
   return (
     <>
-      {/* Only one Header is rendered here */}
       <Header />
       <Outlet />
       {/* <Chat /> */}
